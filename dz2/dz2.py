227b45f388ec35f3abae6041ebedb72ac656243b
ZH <zharov.2010@yandex.ru>
2023-10-22 15:38:41 +0300

    dz2

---
+++ dz2/dz2.py
@@ -0,0 +1,14 @@
+# Процедурная парадигма, т.к. нам нужен вывод и возвращать ничего не нужно
+
+
+def table_mult(n):
+
+    for i in range(1, n + 1):
+        x = ""
+        for j in range(1, n + 1):
+            x = x + f"  {i}*{j}={i * j}"
+        print(x+'\n')
+
+
+n = int(input("Введите число n: "))
+table_mult(n)
